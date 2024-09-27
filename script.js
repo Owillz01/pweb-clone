b668938684a353f4ce3249345ed2dba214918c27
Owillz01 <omonkhodion@gmail.com>
2024-09-27 19:21:30 +0100

    added active button toggle

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello, World!");
+let home = document.querySelector("#home");
+let bio = document.querySelector("#bio");
+let experince = document.querySelector("#experince");
+let header = document.querySelector("header");
+let prev_ele_id = 'home_btn'
+
+header.addEventListener("click", (event) => {
+    prev_ele_id ? null : (prev_ele_id = event.target.id);
+    event.target.classList.add("active");
+    if(prev_ele_id != event.target.id){
+      document.querySelector("#" + prev_ele_id).classList.remove('active');
+      //           .target?.classList.remove();
+
+      // prev_ele_id?.target?.classList.remove()
+      console.log(prev_ele_id, event.target.id, "prev_ele_id?.target?.hash ");
+      prev_ele_id = event.target.id;
+    }
+    
+    console.log(event.target.hash, "event.target.hash");
+    
+});
+
+
+// bio.toggleAttribute('aria-current="page"', false);
